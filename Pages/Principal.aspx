65e013b48519b95e69383bdb818887be63053049
Dennis2101991 <dennistote1995@hotmail.com>
2025-03-28 21:44:52 -0600

    Agregue un label

--- Pages/Principal.aspx
+++ Pages/Principal.aspx
@@ -1,6 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Principal.aspx.cs" Inherits="SistemaReservaciones.Pages.Principal" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Proyecto final</h1>
+
+    <asp:Label ID="LblDatos" runat="server" Text="Ingrese los datos"></asp:Label>
+
     <asp:TextBox ID="TextDatos" runat="server" Text="Datos"></asp:TextBox>
 
 </asp:Content>
--- Pages/Principal.aspx.designer.cs
+++ Pages/Principal.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace SistemaReservaciones.Pages
     {
 
         /// <summary>
+        /// Control LblDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblDatos;
+
+        /// <summary>
         /// Control TextDatos.
         /// </summary>
         /// <remarks>
