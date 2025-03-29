da8ae312eb9171c9754a2c27277857810d1d5b70
Dennis2101991 <dennistote1995@hotmail.com>
2025-03-28 21:01:05 -0600

    Segundo cambio

--- Pages/Principal.aspx
+++ Pages/Principal.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Principal.aspx.cs" Inherits="SistemaReservaciones.Pages.Principal" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Proyecto final</h1>
+    <asp:TextBox ID="TextDatos" runat="server" Text="Datos"></asp:TextBox>
 
 </asp:Content>
--- Pages/Principal.aspx.designer.cs
+++ Pages/Principal.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace SistemaReservaciones.Pages
 
     public partial class Principal
     {
+
+        /// <summary>
+        /// Control TextDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextDatos;
     }
 }
