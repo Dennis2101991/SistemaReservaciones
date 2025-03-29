0fd8ebca21f2e988cfa9ac87fdb4748a541a5981
PalmasCR <stifpalma@hotmail.es>
2025-03-28 23:35:55 -0600

    Página Misreservaciones

---
+++ Pages/Misreservaciones.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Misreservaciones.aspx.cs" Inherits="SistemaReservaciones.Pages.Misreservaciones" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Mis reservaciones</h1>
+    <div style="margin-bottom: 10px">
+        <%--Redirige a la página para realizar una nueva reservación por medio del formulario --%>
+        <a href="NuevaReservacion.aspx">Nueva reservación</a> 
+    </div>
+
+    <div>
+        <asp:HiddenField ID="hdnidReservacion" runat="server" /> <%-- Código oculto--%>
+        <div>
+            <span>Hotel</span>
+            <asp:TextBox ID="txtHotel" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div>
+            <span>Fecha Entrada</span>
+            <asp:TextBox ID="txtFechaEntrada" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div>
+            <span>Fecha Salida</span>
+            <asp:TextBox ID="txtFechaSalida" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div>
+            <span>Costo</span>
+            <asp:TextBox ID="txtCosto" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div>
+            <span>Estado</span>
+            <asp:TextBox ID="txtEstado" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Pages/Misreservaciones.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SistemaReservaciones.Pages
+{
+
+
+    public partial class Misreservaciones
+    {
+
+        /// <summary>
+        /// Control hdnidReservacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnidReservacion;
+
+        /// <summary>
+        /// Control txtHotel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHotel;
+
+        /// <summary>
+        /// Control txtFechaEntrada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaEntrada;
+
+        /// <summary>
+        /// Control txtFechaSalida.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaSalida;
+
+        /// <summary>
+        /// Control txtCosto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCosto;
+
+        /// <summary>
+        /// Control txtEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEstado;
+    }
+}
